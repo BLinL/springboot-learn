a473f445fe040a087bca1cd5d53235415573f985
bliu <1073842723@qq.com>
2021-09-07 16:20:39 +0800

    change config

--- cache-demo/src/main/java/com/example/cache/demo/controller/UserController.java
+++ cache-demo/src/main/java/com/example/cache/demo/controller/UserController.java
@@ -4,9 +4,11 @@ import com.example.cache.demo.pojo.User;
 import com.example.cache.demo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.CacheManager;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 public class UserController {
@@ -18,7 +20,7 @@ public class UserController {
 //    private CacheManager cacheManager;
 
     @GetMapping("api/user/{username}")
-    public User getUser(@PathVariable String username){
+    public ResponseEntity<User> getUser(@PathVariable String username){
 
 //        System.out.println(cacheManager.getClass());
 
@@ -28,6 +30,33 @@ public class UserController {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        return user;
+
+
+        return new ResponseEntity<>(user, HttpStatus.OK);
+    }
+
+    @PutMapping("api/user")
+    public String update(User user) {
+        System.out.println("request params" + user);
+        try {
+            userService.UpdateUserByUsername(user);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        return "{status: ok}";
+    }
+
+    @PostMapping("api/user")
+    public User add(User user) {
+        User resp = null;
+        System.out.println("request params" + user);
+        try {
+            resp = userService.addUser(user);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        return resp;
     }
 }
