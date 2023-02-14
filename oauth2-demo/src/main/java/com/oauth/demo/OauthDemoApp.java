01ccd8375d97d136dd71fa540488c80abe1193bc
baolin <1073842723@qq.com>
2023-02-14 23:09:27 +0800

    oauth2 demo

---
+++ oauth2-demo/src/main/java/com/oauth/demo/OauthDemoApp.java
@@ -0,0 +1,11 @@
+package com.oauth.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthDemoApp {
+    public static void main(String[] args) {
+        SpringApplication.run(OauthDemoApp.class, args);
+    }
+}
